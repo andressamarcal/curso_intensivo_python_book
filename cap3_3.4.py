b2fe24b00929ae23125aed898f7484a496246492
Andressa Marçal <andressagb@gmail.com>
2018-03-13 11:06:37 -0300

    Exercise chapter 3, point 3.4

---
+++ cap3_3.4.py
@@ -0,0 +1,6 @@
+peoples = ['andressa','aninha','wesley']
+message = "Hi " + peoples[0] + " welcome to my dinner!"
+message2 = "Hi " + peoples[1] + " welcome to my dinner!"
+message3 =  "Hi " + peoples[2] + " welcome to my dinner!"
+
+print(message, message2, message3)
