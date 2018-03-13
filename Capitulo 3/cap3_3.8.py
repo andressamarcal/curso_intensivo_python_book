7f44c3d421fb42e8dee1b045ad5b1e742418e08c
Andressa Marçal <andressagb@gmail.com>
2018-03-13 18:39:48 -0300

    Add 3.8 and 3.9

---
+++ Capitulo 3/cap3_3.8.py
@@ -0,0 +1,12 @@
+places = ['peru', 'chile', 'patagonia', 'irlanda', 'canada']
+print(places)
+print(sorted(places))
+print(places)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
---
+++ Capitulo 3/cap3_3.9.py
@@ -0,0 +1,5 @@
+peoples = ['andressa','aninha','wesley']
+print(len(peoples))
+
+places = ['peru', 'chile', 'patagonia', 'irlanda', 'canada']
+print(len(places))
