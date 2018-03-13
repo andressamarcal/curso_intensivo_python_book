63e53045491dc05525d7736e27407f3d2abbaaed
Andressa Marçal <andressagb@gmail.com>
2018-03-13 14:39:25 -0300

    ...

--- Capitulo 3/cap3_3.4.py
+++ Capitulo 3/cap3_3.4.py
@@ -1,4 +1,4 @@
-peoples = ['andressa','aninha','wesley']
+peoples = ['andressa','ana','wesley']
 message = "Hi " + peoples[0] + " welcome to my dinner!"
 message2 = "Hi " + peoples[1] + " welcome to my dinner!"
 message3 =  "Hi " + peoples[2] + " welcome to my dinner!"
