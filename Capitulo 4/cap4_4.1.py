b0f17b2d9ef7f4698d07dd4fba6c4d73914e4ba4
Andressa Marçal <andressagb@gmail.com>
2018-03-13 20:07:56 -0300

    Add 4.1 and 4.2

---
+++ Capitulo 4/cap4_4.1.py
@@ -0,0 +1,5 @@
+pizzas = ['portuguesa','frango com catupiry','4 queijos']
+
+for pizza in pizzas:
+    print("Hmm, gosto de pizza " + pizza.title())
+print("Eu realmente adoro pizza!")
---
+++ Capitulo 4/cap4_4.2.py
@@ -0,0 +1,5 @@
+animals = ['gato','cachorro','coelho']
+
+for animal in animals:
+    print("Um " +animal.title()+ " seria uma otima opção de animal de estimação" )
+print("Qualquer um desses animais seriam ótimos animais de estimação")
