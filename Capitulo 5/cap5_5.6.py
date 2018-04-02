28854048179e558aae94f4a7c24beb34fe3a5ce4
Andressa Marçal <andressagb@gmail.com>
2018-04-01 21:14:05 -0300

    Up

---
+++ Capitulo 5/cap5_5.6.py
@@ -0,0 +1,14 @@
+age = 25
+
+if age < 2:
+    print('His a baby')
+elif (age == 2) or (age < 4):
+    print('His a children')
+elif age == 4 or age < 13:
+    print('His a boy/girl')
+elif age == 13 or age < 20:
+    print('his a teenager')
+elif age == 20 or age < 65:
+    print('His a adult')
+else:
+    print('old')
